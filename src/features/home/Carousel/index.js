8a2bee8ccfeea16ffc26e84e433116c67fd00381
KYLENN K Y HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGKY3@OOCL.COM>
2022-08-08 16:23:15 +0800

    #32 fix: fix a bug use effect send request

--- src/features/home/Carousel/index.js
+++ src/features/home/Carousel/index.js
@@ -2,15 +2,20 @@
 import {Carousel} from 'antd';
 import './index.css'
 import {getCarousels} from '../../../api/home'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {useDispatch} from "react-redux";
 
 export default () => {
     const [carousels, setCarousels] = useState([])
-    getCarousels().then(response => {
-        setCarousels(response.data.data.carousel);
-    }).catch(function (msg) {
-        console.log(msg);
-    })
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        getCarousels().then(response => {
+            setCarousels(response.data.data.carousel);
+        }).catch(function (msg) {
+            console.log(msg);
+        })
+    },[dispatch]);
+
     return (
         <div>
             <Carousel autoplay>
