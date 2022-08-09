e6ed4d59b61768138c72857563e3b45d4657ca0c
blank-1024 <13380497335@163.com>
2022-08-09 17:47:11 +0800

    feat: add searchPage Input function

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,32 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import {useLocation} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {useLocation} from "react-router-dom";
 import './index.css'
 import searchApi from '../../api/searchApi'
 import MovieCard from "../../features/MovieCard";
-import { Pagination  } from 'antd';
+import {Pagination} from 'antd';
+import {Empty} from "antd";
 
-export default function SearchPage (props) {
+export default function SearchPage(props) {
     const location = useLocation()
     const {state} = location
     const [totalData, setTotalData] = useState({});
     const [currPage, setCurrPage] = useState(1);
     const [movies, setMovies] = useState([]);
+    const [input, setInput] = useState("");
 
-    useEffect(()=>{
-        searchApi.searchCinemasOrMovies({keyword : state.keyword,
-            pageSize:6})
-            .then(res=>{
+    useEffect(() => {
+        searchApi.searchCinemasOrMovies({
+            keyword: state.keyword,
+            pageSize: 6
+        })
+            .then(res => {
                 setTotalData(res.data.data.movies);
                 setMovies(res.data.data.movies.content)
             })
-    },[state])
+    }, []);
+
+    const getInput = (event) => {
+        setInput(event.target.value)
+    };
+
+    const onSearch = () => {
+        searchApi.searchCinemasOrMovies({
+            keyword: input,
+            pageSize: 6
+        })
+            .then(res => {
+                setTotalData(res.data.data.movies);
+                setMovies(res.data.data.movies.content)
+            })
+    }
 
     const changePage = (page, pageSize) => {
-        searchApi.searchCinemasOrMovies({keyword : state.keyword,
-            page:page,
-            pageSize:pageSize})
-            .then(res=>{
-                console.log(res)
+        searchApi.searchCinemasOrMovies({
+            keyword: state.keyword,
+            page: page,
+            pageSize: pageSize
+        })
+            .then(res => {
                 setTotalData(res.data.data.movies);
                 setMovies(res.data.data.movies.content)
             })
@@ -35,22 +55,31 @@ export default function SearchPage (props) {
 
     return (
         <>
-            <div className="movie-list-card-col">
+            <div className="header_style">
+                <div className="input_button">
+                    <input onChange={getInput} className="input_style" placeholder="搜电影，演职员" value={input}/>
+                    <a className="button_style" type="submit" onClick={onSearch}></a>
+                </div>
+            </div>
+            {movies.length === 0 ? <Empty className="empty_style"/> : <div className="movie-list-card-col">
                 {
                     movies.map((item, idx) => (
                         <MovieCard movie={item} key={idx}/>
                     ))
                 }
             </div>
-            <div className="movie-list-pagination">
-                <Pagination
+            }
+            <div>
+                {movies.length !== 0 &&
+                <div className="movie-list-pagination"><Pagination
                     total={totalData.totalElements}
                     showTotal={(total) => `Total ${total} 条数据`}
                     defaultPageSize={6}
                     current={currPage}
                     defaultCurrent={1}
                     onChange={changePage}
-                />
+                /></div>
+                }
             </div>
         </>
 
