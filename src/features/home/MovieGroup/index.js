91ec7e26d8d604cf61cddeac493831e9afe9f11a
blank-1024 <13380497335@163.com>
2022-08-09 09:07:30 +0800

    # 40 feat:Home component integration rendering

--- src/features/home/MovieGroup/index.js
+++ src/features/home/MovieGroup/index.js
@@ -25,13 +25,17 @@ export default function MovieGroup(props) {
             <Col span={6} key={movie.id}>
               <Card
                 hoverable
-                style={{ width: 200 }}
-                cover={<img alt="example" src={movie.cover} />}
+                cover={
+                    <div className="image_style">
+                        <img alt="example" src={movie.cover} style={{width: 180}} />
+                    </div>
+                }
+                style={{ width: 180 }}
                 onClick={() => jumpToMovieDetail(movie.id)}
               >
                 <div className="movie_desc">
-                  <div>{movie.movieName}</div>
-                  <div className="movie_score">{movie.score}</div>
+                    <div><b>{movie.movieName}</b></div>
+                    <div className="movie_score"><b>{movie.score}</b></div>
                 </div>
                 <MovieItem key={movie.id} movie={{ movie }} />
               </Card>
